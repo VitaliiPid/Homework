c5ac2d5a108e12ed12a568d7d84e8743f423bb79
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-12-05 10:05:37 +0100

    Homework 18

--- src/Lesson_18/Homework/Student.java
+++ src/Lesson_18/Homework/Student.java
@@ -10,9 +10,9 @@ public class Student {
     }
 
     public static void main(String[] args) {
-        Student student1 = new Student("Alice", 20);
-        Student student2 = new Student("Amy", 22);
-        Student student3 = new Student("Charlie", 21);
+        Student student1 = new Student("Alice", 28);
+        Student student2 = new Student("Amy", 32);
+        Student student3 = new Student("Chris", 40);
 
         System.out.print("The name " + student1.name + ", age is: " + student1.age + "\n");
         System.out.print("The name " + student2.name + ", age is: " + student2.age + "\n");
