2bb7b7c13f29d18c77825b739159b9add4e97d1c
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-12-05 09:07:16 +0100

    Homework 18

---
+++ src/Lesson_18/Homework/Student.java
@@ -0,0 +1,22 @@
+package Lesson_18.Homework;
+
+public class Student {
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 20);
+        Student student2 = new Student("Amy", 22);
+        Student student3 = new Student("Charlie", 21);
+
+        // Вывод информации о студентах на печать без использования return
+        System.out.print("The name " + student1.name + ", age is: " + student1.age + "\n");
+        System.out.print("The name " + student2.name + ", age is: " + student2.age + "\n");
+        System.out.print("The name " + student3.name + ", age is: " + student3.age + "\n");
+    }
+}
