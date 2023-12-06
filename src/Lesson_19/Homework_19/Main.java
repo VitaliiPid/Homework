423c6404a654c8d7e87dfcd9f1e79bf4eecb72c7
VitaliiPid <vitalii.piddubnyi@gmail.com>
2023-12-06 09:10:50 +0100

    Homework 19

--- src/Lesson_19/Homework_19/Main.java
+++ src/Lesson_19/Homework_19/Main.java
@@ -1,6 +1,12 @@
 package Lesson_19.Homework_19;
 
 public class Main {
+    /*
+    Создать класс Temperature (температура), содержащий метод для перевода градусов по фаренгейту
+    в градусы цельсия ( celsius = 5*(farenheit - 32)/9). В классе Main создать несколько обьектов
+    класса Temperature (значение температуры по фаренгейту должно задаваться через конструктор)
+    и вывести результат перевода на экран
+     */
     public static void main(String[] args) {
         Temperature temperature1 = new Temperature(32);
         Temperature temperature2 = new Temperature(184);
